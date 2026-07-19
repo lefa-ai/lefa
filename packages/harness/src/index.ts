b4dc8e507ea3088f10539d7ca9c7297a654dfeb4
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-07-19 16:09:59 -0700

    Export harness read tool

--- packages/harness/src/index.ts
+++ packages/harness/src/index.ts
@@ -1,1 +1,12 @@
-export {}
+export {
+  createReadTool,
+  readTextFile,
+  DEFAULT_MAX_BYTES,
+  DEFAULT_MAX_LINES,
+  type ReadTextFileInput,
+  type ReadTool,
+  type ReadToolDetails,
+  type ReadToolInput,
+  type ReadToolResult,
+  type TruncationResult
+} from './read.ts'
