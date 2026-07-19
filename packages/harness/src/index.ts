4ae3575ae87fa29189b21e758e64ad1d36468134
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-07-19 16:14:26 -0700

    Narrow harness read exports

--- packages/harness/src/index.ts
+++ packages/harness/src/index.ts
@@ -1,12 +1,1 @@
-export {
-  createReadTool,
-  readTextFile,
-  DEFAULT_MAX_BYTES,
-  DEFAULT_MAX_LINES,
-  type ReadTextFileInput,
-  type ReadTool,
-  type ReadToolDetails,
-  type ReadToolInput,
-  type ReadToolResult,
-  type TruncationResult
-} from './read.ts'
+export { createReadTool } from './read.ts'
