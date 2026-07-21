a4afbb89ca316ccfb9d853737ebe9b1378e37519
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-07-20 20:41:36 -0700

    Register bash tool

--- packages/harness/src/index.ts
+++ packages/harness/src/index.ts
@@ -1,14 +1,18 @@
 import { ToolLoopAgent, type LanguageModel, type ToolSet } from 'ai'
+import { createBashTool, type BashTool } from './bash.ts'
 import { createReadTool, type ReadTool } from './read.ts'
 
+export { createBashTool, type BashInput, type BashOutput, type BashTool } from './bash.ts'
 export { createReadTool, type ReadInput, type ReadOutput, type ReadTool } from './read.ts'
 
 export interface HarnessTools extends ToolSet {
+  bash: BashTool
   read: ReadTool
 }
 
 export function createTools(cwd: string): HarnessTools {
   return {
+    bash: createBashTool(cwd),
     read: createReadTool(cwd)
   }
 }
