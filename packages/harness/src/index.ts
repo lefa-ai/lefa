8afd3b27e47fe379a5d8579c36ad56cfc1e20dd1
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-07-20 21:08:09 -0700

    Register edit tool

--- packages/harness/src/index.ts
+++ packages/harness/src/index.ts
@@ -1,18 +1,22 @@
 import { ToolLoopAgent, type LanguageModel, type ToolSet } from 'ai'
 import { createBashTool, type BashTool } from './bash.ts'
+import { createEditTool, type EditTool } from './edit.ts'
 import { createReadTool, type ReadTool } from './read.ts'
 
 export { createBashTool, type BashInput, type BashOutput, type BashTool } from './bash.ts'
+export { createEditTool, type EditInput, type EditOutput, type EditTool } from './edit.ts'
 export { createReadTool, type ReadInput, type ReadOutput, type ReadTool } from './read.ts'
 
 export interface HarnessTools extends ToolSet {
   bash: BashTool
+  edit: EditTool
   read: ReadTool
 }
 
 export function createTools(cwd: string): HarnessTools {
   return {
     bash: createBashTool(cwd),
+    edit: createEditTool(cwd),
     read: createReadTool(cwd)
   }
 }
